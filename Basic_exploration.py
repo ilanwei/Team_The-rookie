69ffae61a45317bdcdf383f8c04a8c480d9983dd
Dheeraj Agarwal <dheeeraj.agarwal@gmail.com>
2016-05-12 15:06:19 -0400

    updated files

---
+++ Basic_exploration.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 10:26:58 2016
+
+@author: OUJ070
+"""
+# Names of columns
+meta_movies_df.columns.values
+mojo_movies_df.columns.values
+
+# total elements and structure of Meta Critic data
+meta_movies_df.size
+meta_movies_df.shape
+
+# total elements and structure of Mojo
+mojo_movies_df.size
+mojo_movies_df.shape
--- data_cleanup.py
+++ data_cleanup.py
@@ -14,6 +14,7 @@ def clean(dataframe, columnname):
     dataframe[columnname].replace("'", "",inplace=True, regex = True)
     dataframe[columnname].replace("!", "",inplace=True, regex = True)
     dataframe[columnname].replace("-", "",inplace=True, regex = True)
+    dataframe[columnname].replace("\\?", "",inplace=True, regex = True)
     dataframe[columnname].replace("\([^)]*\)", "",inplace=True, regex = True)
     dataframe[columnname].replace(" ", "",inplace=True, regex = True)
     dataframe[columnname]= dataframe[columnname].str.lower()
@@ -21,7 +22,11 @@ def clean(dataframe, columnname):
 clean(mojo_movies_df, 'title_cln')
 clean(meta_movies_df, 'title_cln')
 
+mojo_clean = mojo_movies_df[pd.notnull(mojo_movies_df['title_cln'])]
+meta_clean = meta_movies_df[pd.notnull(meta_movies_df['title_cln'])]
 
-movie_db = pd.merge(mojo_movies_df, meta_movies_df, on = 'title_cln', suffixes = ('_mojo', '_meta'), how = 'outer')
+movie_db = pd.merge(mojo_clean, meta_clean, on = 'title_cln', suffixes = ('_mojo', '_meta'), how = 'outer')
 
-movie_db.shape
+keepers = ['domestic_gross', 'opening_per_theater', 'opening_weekend_take', 'production_budget', 'title_cln', 'widest_release', 'worldwide_gross', 'year_meta', 'year_mojo', 'director_meta', 'genre', 'metascore', 'num_critic_reviews', 'num_user_ratings', 'num_user_reviews', 'rating', 'release_date', 'runtime_minutes', 'studio', 'user_score']
+
+movie_final = movie_db[keepers]
